02cc5e07f97d7698c041103c5eff8047b019c891
Mariana Rocha <133812120+MarianaRCosta@users.noreply.github.com>
2023-10-28 10:41:42 -0300

    Add files via upload

---
+++ Mod9/IntegerToWrapper.java
@@ -0,0 +1,26 @@
+
+public class IntegerToWrapper {
+
+	public static void main(String[] args) {
+		// Boxing
+		int a = 50;
+		Integer aOBJ = new Integer(a);
+		
+		
+		double b = 5;
+		Double bOBJ = new Double (b);
+		
+		char a = 'a';
+		Character aOBJ = new Character (a);
+		
+		//Unboxing
+		
+		Integer num1 = 10;
+
+		Boolean status = true;
+
+		Byte b = 1;
+		
+	}
+
+}
